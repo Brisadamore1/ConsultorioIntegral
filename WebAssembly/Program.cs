a4c23b8d01a9ffccf68b429ef90e4a6e2bb7482a
Brisa Damore <brisadamore17@gmail.com>
2025-11-26 17:13:50 -0300

    Refactorización y mejoras en componentes Blazor
    Se eliminó la funcionalidad de colapsar el menú de navegación en
    `NavMenu.razor` y se simplificó su estructura. Se añadió un enlace
    a la página principal con un ícono y se mejoró la lógica de
    autenticación.
    
    Se estandarizaron nombres de métodos en `ContactosEmergencia.razor`,
    `Pacientes.razor` y `Turnos.razor`. Se agregaron parámetros para
    gestionar la edición de entidades en componentes como
    `NuevoEditarContactoEmergencia.razor`, `NuevoEditarPaciente.razor`,
    `NuevoEditarProfesional.razor` y `NuevoEditarTurno.razor`.
    
    Se eliminaron redundancias en `Turnos.razor` y `_Imports.razor`.
    En `Program.cs`, se añadieron comentarios para documentar la
    configuración del cliente HTTP y el inicio de la aplicación.
    Finalmente, se añadió un spinner de carga global en `index.html`
    para mejorar la experiencia de usuario.

--- WebAssembly/Program.cs
+++ WebAssembly/Program.cs
@@ -8,9 +8,9 @@ using WebAssembly.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+//Define el componente raiz
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
 
 var host = builder.HostEnvironment;
 
@@ -20,6 +20,7 @@ string apiBaseUrl = remoto
     ? Service.Properties.Resources.UrlApi
     : Service.Properties.Resources.UrlApiLocal;
 
+//Establece el HttpClient para las llamadas a la API
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseUrl) });
 
 builder.Services.AddScoped(typeof(IGenericService<>), typeof(GenericService<>));
@@ -27,4 +28,5 @@ builder.Services.AddScoped<IProfesionalService, ProfesionalService>();
 builder.Services.AddScoped<FirebaseAuthService>();
 builder.Services.AddSweetAlert2();
 
+//Arranca Blazor WebAssembly
 await builder.Build().RunAsync();
